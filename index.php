40a65ecf8950001fd6569f2381b41d8c3f634fe9
Zhandos Saparbayev <74696169+zhandos200103201@users.noreply.github.com>
2022-11-18 15:28:28 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello world</title>
+</head>
+<body>
+<?php
+// 1 task
+//$_to_round = 3.65;
+//$_to_floor = 6.65;
+//$_to_ceil = -11.54;
+//echo round($_to_round, 1) . "<br>";
+//echo floor(floor($_to_floor*10))/10 . "<br>";
+//echo ceil(ceil($_to_ceil*10))/10 . "<br>";
+// 1 task done
+
+// 2 task
+//function getRandomNum() {
+//    $_rand_max = 100;
+//    $_rand_min = 1;
+//    return rand($_rand_min, $_rand_max);
+//}
+//function getRandomOperationNum(){
+//    // + - * / **
+//    $_rand_max = 5;
+//    $_rand_min = 1;
+//    return rand($_rand_min, $_rand_max);
+//}
+//$_random_num1 = getRandomNum();
+//$_random_num2 = getRandomNum();
+//$_random_operate = getRandomOperationNum();
+//$_result = $_random_num1;
+//switch ($_random_operate){
+//    case 1: $_result = $_random_num1 + $_random_num2; $_random_operate = "+"; break;
+//    case 2: $_result = $_random_num1 - $_random_num2; $_random_operate = "-"; break;
+//    case 3: $_result = $_random_num1 * $_random_num2; $_random_operate = "*"; break;
+//    case 4: $_result = $_random_num1 / $_random_num2; $_random_operate = "/"; break;
+//    case 5: $_result = $_random_num1 ** $_random_num2; $_random_operate = "^"; break;
+//}
+//echo "{$_random_num1} {$_random_operate} {$_random_num2} = {$_result}";
+// 2 task done
+
+// 3 task
+//function getRandomOperationNum(){
+//    $_rand_max = 3;
+//    $_rand_min = 1;
+//    return rand($_rand_min, $_rand_max);
+//}
+//function getRPS($_num){
+//    switch ($_num){
+//        case 1: return "Rock"; break;
+//        case 2: return "Paper"; break;
+//        case 3: return "Scissors"; break;
+//    }
+//}
+//function getWinner($_Anna, $_Akerke){
+//    $_result = 0;
+//    if($_Anna + $_Akerke == 3 || $_Anna + $_Akerke == 5){
+//        if($_Anna < $_Akerke)
+//            $_result = "Akerke";
+//        else
+//            $_result = "Anna";
+//    } else if($_Anna + $_Akerke == 4){
+//        if($_Anna > $_Akerke)
+//            $_result = "Akerke";
+//        else
+//            $_result = "Anna";
+//    } else {
+//        $_result = "Nobody";
+//    }
+//    echo "Anna: " . getRPS($_Anna)." vs Akerke: " . getRPS($_Akerke) . " Winner is " . $_result;
+//}
+//$_Anna = getRandomOperationNum();
+//$_Akerke = getRandomOperationNum();
+//getWinner($_Anna, $_Akerke);
+// 3 task is done
+
+// 4 task
+//function getRandomOperationNum(){
+//    $_rand_max = 10;
+//    $_rand_min = 1;
+//    return rand($_rand_min, $_rand_max);
+//}
+//$_first_num = getRandomOperationNum();
+//$_second_num = getRandomOperationNum();
+//echo "Before changing First num is " . $_first_num . " Second num is " . $_second_num . "<br>";
+//$_first_num += $_second_num;
+//$_second_num = $_first_num - $_second_num;
+//$_first_num = $_first_num - $_second_num;
+//echo "After changing First num is " . $_first_num . " Second num is " . $_second_num;
+// 4 task is done
+?>
+</body>
+</html>
